e761dc481634ae997c75c47e24baba83d47f84e6
Gar <gar+gh@danger.computer>
2017-10-30 10:22:34 -0700

    Greenkeeper/code 5.1.0 (#38)
    * chore(package): update code to version 5.1.0
    
    Closes #35
    
    * no more `done`

--- test/default.js
+++ test/default.js
@@ -9,63 +9,56 @@ var lab = exports.lab = Lab.script();
 //We format the results so the tests are easier to read
 lab.experiment('default tests', function () {
 
-  lab.test('Single year', function (done) {
+  lab.test('Single year', function () {
 
     var result = Ambit.ambit('2005');
     Code.expect(result, 'parsed year').to.include(['start', 'end']);
     Code.expect(result.start.format('YYYY-MM-DD HH:mm:ss'), 'start date').to.equal('2005-01-01 00:00:00');
     Code.expect(result.end.format('YYYY-MM-DD HH:mm:ss'), 'end date').to.equal('2005-12-31 23:59:59');
-    done();
   });
-  lab.test('Year range', function (done) {
+  lab.test('Year range', function () {
 
     var result = Ambit.ambit('2005 to 2007');
     Code.expect(result, 'parsed year').to.include(['start', 'end']);
     Code.expect(result.start.format('YYYY-MM-DD HH:mm:ss'), 'start date').to.equal('2005-01-01 00:00:00');
     Code.expect(result.end.format('YYYY-MM-DD HH:mm:ss'), 'end date').to.equal('2007-12-31 23:59:59');
-    done();
   });
-  lab.test('Two digit year', function (done) {
+  lab.test('Two digit year', function () {
 
     var result = Ambit.ambit('05');
     Code.expect(result, 'parsed year').to.include(['start', 'end']);
     Code.expect(result.start.format('YYYY-MM-DD HH:mm:ss'), 'start date').to.equal('2005-01-01 00:00:00');
     Code.expect(result.end.format('YYYY-MM-DD HH:mm:ss'), 'end date').to.equal('2005-12-31 23:59:59');
-    done();
   });
-  lab.test('Single month with year', function (done) {
+  lab.test('Single month with year', function () {
 
     var result = Ambit.ambit('March 2005');
     Code.expect(result, 'parsed year').to.include(['start', 'end']);
     Code.expect(result.start.format('YYYY-MM-DD HH:mm:ss'), 'start date').to.equal('2005-03-01 00:00:00');
     Code.expect(result.end.format('YYYY-MM-DD HH:mm:ss'), 'end date').to.equal('2005-03-31 23:59:59');
-    done();
   });
-  lab.test('Month with year range', function (done) {
+  lab.test('Month with year range', function () {
 
     var result = Ambit.ambit('March 2005 to August 2005');
     Code.expect(result, 'parsed year').to.include(['start', 'end']);
     Code.expect(result.start.format('YYYY-MM-DD HH:mm:ss'), 'start date').to.equal('2005-03-01 00:00:00');
     Code.expect(result.end.format('YYYY-MM-DD HH:mm:ss'), 'end date').to.equal('2005-08-31 23:59:59');
-    done();
   });
-  lab.test('Season with year', function (done) {
+  lab.test('Season with year', function () {
 
     var result = Ambit.ambit('Spring 2005');
     Code.expect(result, 'parsed year').to.include(['start', 'end']);
     Code.expect(result.start.format('YYYY-MM-DD'), 'start date').to.equal('2005-03-20');
     Code.expect(result.end.format('YYYY-MM-DD'), 'end date').to.equal('2005-06-21');
-    done();
   });
-  lab.test('Season crossing year boundary', function (done) {
+  lab.test('Season crossing year boundary', function () {
 
     var result = Ambit.ambit('Winter 2005');
     Code.expect(result, 'parsed year').to.include(['start', 'end']);
     Code.expect(result.start.format('YYYY-MM-DD'), 'start date').to.equal('2005-12-21');
     Code.expect(result.end.format('YYYY-MM-DD'), 'end date').to.equal('2006-03-20');
-    done();
   });
-  lab.test('Months', function (done) {
+  lab.test('Months', function () {
 
     var months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
     months.forEach(function (month) {
@@ -75,16 +68,14 @@ lab.experiment('default tests', function () {
       Code.expect(result.start, 'start of ' + month).to.be.below(result.end, 'end of ' + month);
       Code.expect(result.end, 'end of ' + month).to.be.above(new Date(), 'now');
     });
-    done();
   });
-  lab.test('Months that are backwards', function (done) {
+  lab.test('Months that are backwards', function () {
 
     var result = Ambit.ambit('oct to sep');
     Code.expect(result, 'parsed months').to.include(['start', 'end']);
     Code.expect(result.start, 'oct').to.be.below(result.end, 'sep');
-    done();
   });
-  lab.test('Seasons', function (done) {
+  lab.test('Seasons', function () {
 
     var seasons = ['spring', 'summer', 'fall', 'autumn', 'winter'];
     seasons.forEach(function (season) {
@@ -94,23 +85,20 @@ lab.experiment('default tests', function () {
       Code.expect(result.start, 'start of ' + season).to.be.below(result.end, 'end of ' + season);
       Code.expect(result.end, 'end date of ' + season).to.be.above(new Date(), 'now');
     });
-    done();
   });
-  lab.test('Season w/o year crossing year boundary', function (done) {
+  lab.test('Season w/o year crossing year boundary', function () {
 
     var result = Ambit.ambit('Winter');
     Code.expect(result, 'parsed season').to.include(['start', 'end']);
     Code.expect(result.end, 'end').to.be.above(result.start, 'start');
-    done();
   });
-  lab.test('Formatting', function (done) {
+  lab.test('Formatting', function () {
 
     var result = Ambit.ambit('march 12, 2005', 'YYYY-MM-DD');
     Code.expect(result, 'parsed year').to.include(['start', 'end']);
     Code.expect(result.start).to.equal('2005-03-12');
-    done();
   });
-  lab.test('Month day', function (done) {
+  lab.test('Month day', function () {
 
     var result = Ambit.ambit('may 12');
     Code.expect(result, 'parsed month day').to.include(['start', 'end']);
@@ -118,9 +106,8 @@ lab.experiment('default tests', function () {
     Code.expect(result.start.month(), 'parsed month start').to.equal(4);
     Code.expect(result.end.date(), 'parsed day end').to.equal(12);
     Code.expect(result.end.month(), 'parsed month end').to.equal(4);
-    done();
   });
-  lab.test('Month two digit year too big to be a day', function (done) {
+  lab.test('Month two digit year too big to be a day', function () {
 
     var result = Ambit.ambit('april 39');
     Code.expect(result, 'parsed month year').to.include(['start', 'end']);
@@ -130,9 +117,8 @@ lab.experiment('default tests', function () {
     Code.expect(result.end.year(), 'parsed year end').to.equal(2039);
     Code.expect(result.end.month(), 'parsed month end').to.equal(3);
     Code.expect(result.end.date(), 'parsed day end').to.equal(30);
-    done();
   });
-  lab.test('Month day to year', function (done) {
+  lab.test('Month day to year', function () {
 
     var now = new Date();
     var year = now.getFullYear() + 1;
@@ -142,9 +128,8 @@ lab.experiment('default tests', function () {
     Code.expect(result.end.date(), 'parsed day end').to.equal(31);
     Code.expect(result.end.month(), 'parsed month end').to.equal(11);
     Code.expect(result.end.year(), 'parsed year end').to.equal(year);
-    done();
   });
-  lab.test('Month \'year', function (done) {
+  lab.test('Month \'year', function () {
 
     var result = Ambit.ambit('may \'12');
     Code.expect(result, 'parsed month day').to.include(['start', 'end']);
@@ -154,18 +139,16 @@ lab.experiment('default tests', function () {
     Code.expect(result.end.date(), 'parsed day end').to.equal(31);
     Code.expect(result.end.month(), 'parsed month end').to.equal(4);
     Code.expect(result.end.year(), 'parsed year end').to.equal(2012);
-    done();
   });
-  lab.test('Full day', function (done) {
+  lab.test('Full day', function () {
 
     var result = Ambit.ambit('March 12, 2001');
     Code.expect(result, 'parsed full day').to.include(['start', 'end']);
     Code.expect(result.start.date(), 'parsed day start').to.equal(12);
     Code.expect(result.start.month(), 'parsed month start').to.equal(2);
     Code.expect(result.start.year(), 'parsed year start').to.equal(2001);
-    done();
   });
-  lab.test('Full day to year', function (done) {
+  lab.test('Full day to year', function () {
 
     var result = Ambit.ambit('September 14, 2002 to 2003');
     Code.expect(result, 'parsed full day').to.include(['start', 'end']);
@@ -175,22 +158,19 @@ lab.experiment('default tests', function () {
     Code.expect(result.end.date(), 'parsed day end').to.equal(31);
     Code.expect(result.end.month(), 'parsed month end').to.equal(11);
     Code.expect(result.end.year(), 'parsed year end').to.equal(2003);
-    done();
   });
-  lab.test('Nonsense', function (done) {
+  lab.test('Nonsense', function () {
 
     var result = Ambit.ambit('first of tocember'); //Octember parses, sorry Dr Suess
     Code.expect(result).to.equal(undefined);
-    done();
   });
-  lab.test('Double ending', function (done) {
+  lab.test('Double ending', function () {
 
     var result = Ambit.ambit('2005 something something');
     Code.expect(result, 'double ending').to.include(['start', 'end']);
     Code.expect(result.start.year(), 'parsed start year').to.equal(2005);
-    done();
   });
-  lab.test('Double separator', function (done) {
+  lab.test('Double separator', function () {
 
     var result = Ambit.ambit('2005 to, like 2007');
     Code.expect(result, 'double separator').to.include(['start', 'end']);
@@ -200,18 +180,15 @@ lab.experiment('default tests', function () {
     Code.expect(result.end.date(), 'parsed end start').to.equal(31);
     Code.expect(result.end.month(), 'parsed end start').to.equal(11);
     Code.expect(result.end.year(), 'parsed end start').to.equal(2007);
-    done();
   });
-  lab.test('Empty string', function (done) {
+  lab.test('Empty string', function () {
 
     var result = Ambit.ambit('');
     Code.expect(result).to.equal(undefined);
-    done();
   });
-  lab.test('Date crash', function (done) {
+  lab.test('Date crash', function () {
 
     var result = Ambit.ambit('275760-09-24');
     Code.expect(result).to.equal(undefined);
-    done();
   });
 });
